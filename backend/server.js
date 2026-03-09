8164aaaedffcfcd4ef709009233e113939c9c854
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2026-03-09 19:32:32 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cors({
   origin: ALLOWED_ORIGINS,
   credentials: true
 }));
-
+console.log("Allowed origins for CORS:", ALLOWED_ORIGINS);
 app.use(helmet());
 app.use(express.json({ limit: "2mb" }));
 app.use(rateLimit);
