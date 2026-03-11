c0309bb5cadd02856a7e8c5c026600c0673ecf39
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2026-03-11 06:19:59 +0200

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const SOCKET_URL = import.meta.env.VITE_API_URL; // use the same env variable
+
 export const socket = io(SOCKET_URL, {
-  autoConnect: false, // okay
+  autoConnect: false,
 });
 
 export const connectSocket = (userId, partnerId) => {
